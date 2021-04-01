d7693c53ab77f71ccb05b86fe8ac329b61cb4cb7
Christina Petroski <cllpetroski@gmail.com>
2021-04-01 14:52:21 -0500

    controllers step 5

---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -0,0 +1,73 @@
+package org.launchcode.javawebdevtechjobspersistent.controllers;
+
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skills")
+public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping
+    public String indexSkills (Model model){
+        model.addAttribute("title", "All Skills");
+        model.addAttribute("categories", skillRepository.findAll());
+        return "skills/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute("title", "Create Skill");
+        model.addAttribute(new Skill());
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/add";
+    }
+
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Skill");
+            return "skills/add";
+        }
+
+        Boolean doesSkillExist=false;
+        var allSkills = skillRepository.findAll();
+        for (Skill skillFromDb : allSkills) {
+            if (skillFromDb.getName().equals(newSkill.getName())){
+                doesSkillExist = true;
+            }
+        }
+        if(doesSkillExist=false) {
+            skillRepository.save(newSkill);
+        }
+        return "redirect:";
+
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+
+        Optional optSkill = skillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Employer employer = (Employer) optSkill.get();
+            model.addAttribute("skill", skill);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+}
